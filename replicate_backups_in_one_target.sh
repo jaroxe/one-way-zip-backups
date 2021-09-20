da8cce5d2e2e1595cc5943fc6d64b50f130b8180
jaroxe <jaroxe@hotmail.com>
2021-09-20 21:50:48 +0200

    replicate_backups_in_one_target.sh

---
+++ replicate_backups_in_one_target.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# replicate backups directory (config value) in specified location (arg 1)
+
+# move to directory that contains the current script (env. variable)
+cd "${OWZB_WHERE}"
+
+# read config inputs (evaluates potential vars in text, e.g.: "${HOME}/backups")
+source config.shlib
+eval BACKUPS_PATH="$(config_get BACKUPS_PATH)"
+
+# read argument (first and only) passed to this file
+eval TARGET_PATH="${1//' '/'\ '}"     # escape spaces for evaluation
+TARGET_PATH="${TARGET_PATH//'\ '/ }"  # put spaces back in place (do not escape)
+
+printf "\t'${TARGET_PATH}':"
+
+# log attempted replication event into 'replications.log' in BACKUPS_PATH
+EVENT="to '${TARGET_PATH}' @ $(date +'%Y-%m-%dT%H-%M')"
+printf "\n${EVENT}" >> "${BACKUPS_PATH}/replications.log"
+
+# if target path cannot be found skip backup replication in this target
+if ! [[ -d "${TARGET_PATH}" ]]; then
+    printf "\n\t\tSKIP TARGET: path could not be found\n"
+    exit 1
+fi
+
+# replicate backups directory inside target (bytes diff only)
+rsync -avq --delete "${BACKUPS_PATH}" "${TARGET_PATH}" && (
+
+    # log SUCCESS of event to replications.log (same line as attempted event)
+    printf ": ## SUCCESS ##" >> "${BACKUPS_PATH}/replications.log"
+
+    # same to stdout
+    printf " ## SUCCESS ##\n"
+)
