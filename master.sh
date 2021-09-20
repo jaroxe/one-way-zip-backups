c86382d2d42d001705b1cbe22775353bcfc0ec73
jaroxe <jaroxe@hotmail.com>
2021-09-20 23:01:07 +0200

    master.sh

---
+++ master.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# back up source directories and replicate backups in target directories
+#   sources: 'sources.txt'
+#   targets: 'additional_targets.txt'
+
+# move to directory that contains the current script (env. variable)
+cd "${OWZB_WHERE}"
+
+sh validate_config.sh && (
+
+    sh back_up_all_sources.sh
+    sh replicate_backups_in_all_targets.sh
+
+)
